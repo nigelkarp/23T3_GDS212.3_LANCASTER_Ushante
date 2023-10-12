254ba9f9994e0e6ab50b70dcb2d737bb82713346
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-13 02:11:46 +1100

    Updating Juice order script
    Updating juice order so that three different recipes can be called. I also created an ingredient scriptable object.

---
+++ Assets/Scripts/ScriptableObjects/Ingredient.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Juice Order", menuName = "Juice Game/Ingredient")]
+
+public class Ingredient : ScriptableObject
+{
+    public string ingredientName;
+
+}
--- Assets/Scripts/ScriptableObjects/JuiceOrder.cs
+++ Assets/Scripts/ScriptableObjects/JuiceOrder.cs
@@ -6,16 +6,29 @@ using UnityEngine;
 
 public class JuiceOrder : ScriptableObject
 {
-    public string orderName;
+    public string recipeName;
     public float orderTime;
     public IngredientQuantity[] requiredIngredients;
 
     [System.Serializable]
     public class IngredientQuantity
     {
-        
+        public Ingredient ingredient;
         public int quantity; //amount of the ingredient needed, i might not use this for now
     }
-    
+
+    [CreateAssetMenu(fileName = "Recipe 1", menuName = "Juice Game/Recipe 1")]
+    public class Recipe1 : JuiceOrder
+    {
+        public Recipe1()
+        {
+            recipeName = "Tropicana";
+            orderTime = 10f;
+            requiredIngredients = new IngredientQuantity[]
+            {
+                new IngredientQuantity { ingredient = Ingredient.Apple}
+            }
+        }
+    }
 }
 
