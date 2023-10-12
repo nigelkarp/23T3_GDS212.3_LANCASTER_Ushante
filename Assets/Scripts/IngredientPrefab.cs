da8c32505a970ca4f99b78b745346ea46fe9eeed
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-13 03:06:41 +1100

    Created ingredientprefab script
    Script responsible for creating a reference to the respective ingredient's SO for later use when handling player clicks/ screen touches

---
+++ Assets/Scripts/IngredientPrefab.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//I need to reference chatgpt for this script
+public class IngredientPrefab : MonoBehaviour
+{
+    public Ingredient ingredient; // Reference to the ingredient scriptable object.
+    
+    //I can add extra logic to change the sprite and what not, but for now its just a reference
+}
