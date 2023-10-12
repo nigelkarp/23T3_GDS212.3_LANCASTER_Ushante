27159a7db01f74a07da9fe12200b179b7821f45b
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-13 05:23:25 +1100

    updated game manager
    added a check recipe method so that when the player presses blend the game checks if the recipe is correct

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,8 @@ public class GameManager : MonoBehaviour
 
     private JuiceOrder _currentRecipe; // Reference to the current recipe
 
+    private List<Ingredient> _selectedIngredients = new List<Ingredient>();
+
     private void Start()
     {
         // Set initial recipe as Recipe 1
@@ -20,7 +22,7 @@ public class GameManager : MonoBehaviour
     public void StartRecipe(JuiceOrder newRecipe)
     {
         _currentRecipe = newRecipe;
-        //set time limit (for now no time), update UI
+        // Set time limit (for now no time), update UI
 
         // Update the UI to display the current order's name
         UpdateUI();
@@ -28,8 +30,8 @@ public class GameManager : MonoBehaviour
 
     public void GiveNextRecipe()
     {
-        //determine which recipe class to instantiate (could be random)
-        //instantiate the chosen recipe class, and call start recipe to begin
+        // Determine which recipe class to instantiate (could be random)
+        // Instantiate the chosen recipe class and call StartRecipe to begin
     }
 
     private void UpdateUI()
@@ -39,7 +41,57 @@ public class GameManager : MonoBehaviour
         {
             _juiceNameText.text = _currentRecipe.recipeName;
         }
+    }
 
+    bool CheckRecipe()
+    {
+        // Get the required ingredients for the current order
+        IngredientQuantity[] requiredIngredients = _currentRecipe.requiredIngredients;
+
+        // Create a dictionary to count the selected ingredients
+        Dictionary<Ingredient, int> selectedIngredientsCount = new Dictionary<Ingredient, int>();
+
+        // Iterate through selected ingredients and count them
+        foreach (Ingredient selectedIngredient in _selectedIngredients)
+        {
+            if (!selectedIngredientsCount.ContainsKey(selectedIngredient))
+            {
+                selectedIngredientsCount[selectedIngredient] = 1;
+            }
+            else
+            {
+                selectedIngredientsCount[selectedIngredient]++;
+            }
+        }
+
+        // Compare the selected ingredients with the required ingredients
+        foreach (IngredientQuantity requiredIngredient in requiredIngredients)
+        {
+            if (!selectedIngredientsCount.ContainsKey(requiredIngredient.ingredient) ||
+                selectedIngredientsCount[requiredIngredient.ingredient] < requiredIngredient.quantity)
+            {
+                Debug.Log("Recipe does not match");
+                return false; // Recipe does not match
+            }
+        }
+
+        Debug.Log("Recipe matches");
+        return true; // Recipe matches
+    }
+
+    public void SelectIngredient(Ingredient ingredient)
+    {
+        // Add the selected ingredient to the list
+        _selectedIngredients.Add(ingredient);
+        Debug.Log("Selected Ingredient: " + ingredient.ingredientName);
+    }
+
+    public void DeselectIngredient(Ingredient ingredient)
+    {
+        // Remove the deselected ingredient from the list
+        _selectedIngredients.Remove(ingredient);
+        Debug.Log("Deselected Ingredient: " + ingredient.ingredientName);
     }
 }
+
 
