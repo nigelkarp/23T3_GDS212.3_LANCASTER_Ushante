15525bdad2fee84f2f6f8f1438391463279d0d71
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-13 01:32:36 +1100

    Updating Ingredient manager
    Changing the script so the player cant drag the ingredients but rather click on them

--- Assets/Scripts/IngredientManager.cs
+++ Assets/Scripts/IngredientManager.cs
@@ -6,91 +6,53 @@ using UnityEngine;
 
 public class IngredientManager : MonoBehaviour
 {
-    private bool _isDragActive = false; //Is player dragging
-    private Vector2 _screenPosition; //Object position on screen
-    private Vector3 _worldPosition;
-    private Draggable _lastDragged;
+    //private bool _isDragActive = false; //Is player dragging
+    //private Vector2 _screenPosition; //Object position on screen
+    //private Vector3 _worldPosition;
+    //private Draggable _lastDragged;
+
+    private bool _isClicked = false; //Is the object clicked
 
     [SerializeField] GameObject _blender;
 
     // Update is called once per frame
     void Update()
     {
-        if (_isDragActive)
+        //if is clicked is true (logic happens)
+        //link scriptable stuff here
+
+        //This cheks for mouse click and touch input
+        if (Input.GetMouseButtonDown(0) || (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began))
         {
-            if (Input.GetMouseButtonDown(0) || (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Ended))
+            Vector3 inputPosition;
+
+            if (Input.touchCount > 0)
             {
-                Drop();
+                inputPosition = Input.GetTouch(0).position;
+            }
+            else
+            {
+                inputPosition = Input.mousePosition;
+            }
+
+            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(inputPosition);
+
+            RaycastHit2D hit = Physics2D.Raycast(worldPosition, Vector2.zero);
+
+            if (hit.collider != null && hit.collider.gameObject == gameObject)
+            {
+                _isClicked = true;
+                IngredientClicked();
+            }
+        }
+
+        void IngredientClicked()
+        {
+            if (_isClicked)
+            {
+                Debug.Log("Ingredient Clicked");
                 return;
             }
-        }
-            
-
-        if (Input.GetMouseButton(0))
-        {
-            Vector3 mousePos = Input.mousePosition;
-            _screenPosition = new Vector2(mousePos.x, mousePos.y);
-
-        } 
-        else if (Input.touchCount > 0)
-        {
-            _screenPosition = Input.GetTouch(0).position;
-        } 
-        else
-        {
-            return;
-        }
-
-        _worldPosition = Camera.main.ScreenToWorldPoint( _screenPosition);
-
-        if (_isDragActive )
-        {
-            Drag(); 
-        }
-        else
-        {
-            RaycastHit2D hit = Physics2D.Raycast(_worldPosition, Vector2.zero );
-            if (hit.collider != null)
-            {
-                Draggable draggable = hit.transform.GetComponent<Draggable>();
-                if (draggable != null)
-                {
-                    _lastDragged = draggable;
-                    InitDrag();
-                }
-            }
-        }
-
-    }
-
-    void InitDrag()
-    {
-        _isDragActive = true;
-    }
-
-    void Drag()
-    {
-        _lastDragged.transform.position = new Vector2(_worldPosition.x, _worldPosition.y);
-    }
-
-    void Drop()
-    {
-        _isDragActive = false;
-    }
-
-    //when this object enters with the blender game object's trigger collider, console log enter
-    void OnTriggerEnter2D(Collider2D other)
-    {
-        //checks if the item has been dropped in the blender
-        if (other.CompareTag("Blender"))
-        {
-            this.gameObject.SetActive(false);
-            //check that its the right fruit
-
-        }
-        else
-        {
-            return;
         }
     }
 
