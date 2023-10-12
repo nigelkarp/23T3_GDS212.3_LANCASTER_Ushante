33c1cad8ecfd0347c83631670f0d659106cb5ae3
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-13 04:02:54 +1100

    Update GameManager.cs
    First recipe is recipe 1

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -12,21 +12,22 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        // Set initial recipe
-
-        // Update the UI to display the current order's name
-        UpdateUI();
+        // Set initial recipe as Recipe 1
+        StartRecipe(new JuiceOrder.Recipe1());
     }
 
     public void StartRecipe(JuiceOrder newRecipe)
     {
         _currentRecipe = newRecipe;
-        //set time limit, update UI
+        //set time limit (for now no time), update UI
+
+        // Update the UI to display the current order's name
+        UpdateUI();
     }
 
     public void GiveNextRecipe()
     {
-        //determine which recipe class to instantiate
+        //determine which recipe class to instantiate (could be random)
         //instantiate the chosen recipe class, and call start recipe to begin
     }
 
