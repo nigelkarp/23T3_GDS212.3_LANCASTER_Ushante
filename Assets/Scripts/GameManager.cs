4d36a8965cf45b52865e7e58c1b4e3368482545b
subway-aint-fresh <97781497+subway-aint-fresh@users.noreply.github.com>
2023-10-13 03:28:30 +1100

    Creating a gameprogress scriptable object
    this is responsible for tracking the games progress (what level and recipe is on)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       //set initial recipe
+       //current game progression?? e.g. Juice order. recipe 1 for game progress. level 1?
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ScriptableObjects/GameProgress.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameProgress : ScriptableObject
+{
+    public string level;
+    public int levelNumber;
+
+    //refence to juice order recipe 
+
+}
